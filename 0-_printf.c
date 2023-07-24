677458a7375de5f1b7b2733e1b28e865d386ee28
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-24 11:44:22 -0700

    dsadas

--- 0-_printf.c
+++ 0-_printf.c
@@ -9,13 +9,29 @@
 int _printf(const char *format, ...)
 {
 	va_list valist;
+	int x;
 	int i = 0, count = 0;
 
 	va_start(valist, format);
 	while (format[i] && format)
 	{
-		write(1, &format[i], 1);
-		count++;
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					x = va_arg(valist, int);
+					write(1, &x, 1);
+					count++;
+			
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
 		i++;
 	}
 	va_end(valist);
